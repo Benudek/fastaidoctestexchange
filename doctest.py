d4c8a5ffdfb4db802fecbd3afb57ad5a8bbb0dae
benedikt herudek <Benedikt.herudek@gmail.com>
2019-02-13 00:19:51 +0100

    Update doctest.py

--- doctest.py
+++ doctest.py
@@ -1,6 +1,5 @@
-import pytest, sys, re, json, inspect
-from os.path import abspath, dirname
-from utils.mem import use_gpu
+import sys, inspect, re
+from os.path import basename, split
 
 __all__ = ['this_tests']
 
@@ -8,30 +7,48 @@ def this_tests(testedapi):
      RegisterTestsperAPI.this_tests(testedapi)
 
 def full_name_with_qualname(klass):
-     return f'{klass.__module__}.{klass.__qualname__}' ##  __name__ for of qualname for short version
+     return f'{klass.__module__}.{klass.__qualname__}' 
 
 def set_default(obj):
      if isinstance(obj, set):
           return list(obj)
      raise TypeError 
 
+def get_parent_func(lineno, lines):
+    for idx,l in enumerate(reversed(lines[:lineno])):
+        if re.match(f'^def test', l):
+            return (lineno - (idx+1)), l
+    return None
+
+def get_lines(file):
+    with open(file, 'r') as f:
+          return f.readlines()
+
 class RegisterTestsperAPI:
     apiTestsMap = dict()
     @staticmethod
     def this_tests(testedapi):
-        
+        previous_frame = inspect.currentframe().f_back.f_back 
+        (pathfilename, line_number, test_function_name, lines, index) = inspect.getframeinfo(previous_frame)
+## TO DO: these prints go out later
         print('\n\n\n\n###############################################################')
-
-        previous_frame = inspect.currentframe().f_back.f_back ##.f_back 
-        (filename, line_number, test_function_name, lines, index) = inspect.getframeinfo(previous_frame)
-        list_test = [{'file: '+ filename, 'test: ' + test_function_name}]
-        print('\n\n############ previous_frame: filename =' + filename)
+        print('\n\n############ previous_frame: filename =' + basename(pathfilename))
         print('\n############ previous_frame: line_number =' + str(line_number))
         print('\n############ previous_frame: test_function_name =' + test_function_name)
         print('\n############ previous_frame: lines =' + str(lines))
         print('\n############ previous_frame: index =' + str(index))
-       
-        print('###############################################################\n\n\n\n')     
+ 
+        
+        ###
+        lineno_parentfunc, parent_func = get_parent_func(line_number, get_lines(pathfilename)) 
+
+        print('\n############ previous_frame: parent_func =' + str(parent_func))
+        print('\n############ previous_frame: lineno_parentfunc =' + str(lineno_parentfunc))
+
+        print('###############################################################\n\n\n\n')    
+        ###
+
+        list_test = [{'file: '+ basename(pathfilename), 'test: ' + test_function_name , 'line: ' + str(lineno_parentfunc)}]
         fq_apiname = full_name_with_qualname(testedapi)
         if(fq_apiname in RegisterTestsperAPI.apiTestsMap):
             RegisterTestsperAPI.apiTestsMap[fq_apiname] = RegisterTestsperAPI.apiTestsMap[fq_apiname]  + list_test
